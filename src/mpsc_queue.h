2766b1b7e5ff3cd551db6f6f4eee8c0735d4887d
Eric Yen <eric@ericyen.com>
2015-10-01 23:01:44 -0400

    use a lock to ensure thread safety.

--- src/mpsc_queue.h
+++ src/mpsc_queue.h
@@ -1,6 +1,5 @@
 #define MPSC_QUEUE_EOF -1
 #define MPSC_QUEUE_FULL -2
-
 #include <atomic>
 #include <thread>
 #include <chrono>
@@ -11,8 +10,6 @@
 This is a very simple MPSC queue using a ring_buffer.
 This is meant to be used in conjunction with fusejs
 
-tail points to the object before 
-head points to the object after
 */
 
 template <typename T>
@@ -23,7 +20,7 @@ public:
 		data = (T*) malloc(ring_size * sizeof(T));
 		if(data == nullptr){
 			//TODO: thow error about not enough memory;
-			printf("WE WERE NOT ABLE TO ALLOCATE MEMOMRY FOR THE QUEUE! \n");
+			// printf("WE WERE NOT ABLE TO ALLOCATE MEMOMRY FOR THE QUEUE! \n");
 			return;
 		}
 
@@ -39,135 +36,135 @@ public:
 		free(data);
 	}
 
-	uint consume(T **value){
+	int consume(T **value){
 		/* 
 		This function returns the next item to be consumed
 		*/
-		volatile uint _tail = (tail + 1) & ring_mask;
-
-		volatile uint _head = head;
-		if( _head != _tail  ){
-			tail = ( tail + 1) & ring_mask; 
-			*value = &(data[_tail]);
-
-			#ifdef DEBUG
-			volatile uint _head = head;
-			volatile uint _claimed = claimed;
-			volatile uint _next = next_to_be_claimed;
-			printf("consumed tail %u -- claimed %u -- head %u -- next %u-- thread id %u\n", 
-				_tail, _claimed, _head,_next,std::this_thread::get_id());
-			#endif
-
-			return 0;
+		mutex_lock.lock();
+		// printf("consume lock\n");
+		if( isEmpty()){
+			// printf("consume unlock\n");
+			mutex_lock.unlock();
+			return MPSC_QUEUE_EOF;			
 		}
-		return MPSC_QUEUE_EOF;
+
+		uint idx = (tail + 1 ) & ring_mask;
+		tail = idx;
+
+		*value = &(data[idx]);
+		// printf("consume unlock\n");
+		mutex_lock.unlock();
+
+		return idx;
+
 	}
 
-	int64_t producer_claim_next(T **value){
+	bool isEmpty(){
+		return head == tail;
+	}
+
+	bool isFull(){
+		
+		/*
+			the queue is full when next_to_be_claimed + 1 is tail.
+			if we defined full as next_to_be_claimed == tail, it could lead to situation where we enqueue something 
+			at the tail position without consuming it.
+		*/
+		return ( 
+			(next_to_be_claimed + 1) & ring_mask 
+		) == tail;
+
+	}
+
+	int producer_claim_next(T **value){
 		mutex_lock.lock();
+		// printf("next lock\n");
 
-		volatile uint _tail;
-		uint idx;
-
-		do
+		while ( isFull() )
 		{
-			_tail =  tail;
-			idx = next_to_be_claimed;
-			_tail = _tail & ring_mask;
-			idx = idx & ring_mask;
 
-			if ( ( (_tail) & ring_mask ) ==
-				( (idx+1) & ring_mask ) )
-			{
+	        // the queue is full		      
+	        // wait until the queue clears
+			// printf("next unlock\n");
+			mutex_lock.unlock();
+			uv_async_send(&uv_async_handle);
+			std::this_thread::yield();
+			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+			mutex_lock.lock();				
+			// printf("next lock\n");
 
-		        // the queue is full		      
-
-		        // wait until the queue empties
-
-				while( (_tail & ring_mask ) == ((idx+1) & ring_mask )){
-					uv_async_send(&uv_async_handle);
-					std::this_thread::yield();
-					std::this_thread::sleep_for(std::chrono::milliseconds(500));
-					_tail =  tail;
-					_tail = _tail & ring_mask;
-
-				}
-
-			} 
-		}while ( !std::atomic_compare_exchange_strong(&next_to_be_claimed, &idx, (idx+1) & ring_mask ));
-		//!next_to_be_claimed.compare_exchange_strong(  (uint) idx, (uint)(idx + 1) ,std::memory_order_seq_cst,std::memory_order_seq_cst) );
-		// if( tail == idx){
-		// 	do{
-		// 		printf("queue full idx %u\tthread_id %u\n", idx, std::this_thread::get_id());
-		// 		uv_async_send(&uv_async_handle);
-		// 		std::this_thread::yield();
-		// 		idx = next_to_be_claimed;
-		// 	} while (idx == tail);
-		// 	idx = next_to_be_claimed++;
-		// }
-
-		// next_to_be_claimed.fetch_and(ring_mask); 
+		} 
 
 		#ifdef DEBUG
-		uint _claimed =  claimed.fetch_add(1) + 1;
+		uint _claimed =  ++claimed;
 		#else
-		claimed.fetch_add(1);
+		claimed++;
 		#endif
 
+		uint idx = (next_to_be_claimed++) & ring_mask;
+
+		// printf("next unlock\n");
+		mutex_lock.unlock();
 		*value = &(data[idx]);
 
 		#ifdef DEBUG
 		volatile uint _head = head;
 		volatile uint _next = next_to_be_claimed;
-		printf("claimed idx %lu -- claimed %lu -- head %u -- next %u -- tail %u -- thread_id %u\n", 
+		// printf("claimed idx %lu -- claimed %lu -- head %u -- next %u -- tail %u -- thread_id %u\n", 
 			idx, _claimed, _head, _next, tail, std::this_thread::get_id());
 		#endif
 
 		// while(!std::atomic_compare_exchange_strong(&claimed, &_claimed, (_claimed+1)&ring_mask));
-		mutex_lock.unlock();
-		return (int64_t) idx;
+		return (int) idx;
 	}
-	uint producer_publish(volatile int64_t _idx){
+	uint producer_publish(int idx){
 		mutex_lock.lock();
-		uint _claimed = claimed.fetch_sub(1) - 1;
-		volatile uint _head = head;		
-		uint next = next_to_be_claimed;
+		// printf("publish lock\n");
+		claimed--;
 
 		// set a new head when the number claimed is 0
+		if( claimed == 0){
+			
+			head = (next_to_be_claimed + ring_mask - 1)  & ring_mask;		
+			// if( head != next){
+			// 	head = next;
+			// 	//while(!std::atomic_compare_exchange_strong(&head, &next, (next+1)&ring_mask)  );
+			// }
 
-		if( _claimed == 0){
-			if( _head != next){
-				head = next;
-				//while(!std::atomic_compare_exchange_strong(&head, &next, (next+1)&ring_mask)  );
-			}
-
+			// printf("publish unlock\n");
+			mutex_lock.unlock();
 
 			uv_async_send(&uv_async_handle);
 
 
 
+		}else{
+			// printf("publish unlock\n");
+			mutex_lock.unlock();
 		}
 
 		#ifdef DEBUG
 		uint idx = _idx; 
 		_head = head;
 		volatile uint _next = next_to_be_claimed;
-		printf("publish idx %u -- claimed %u -- head %u -- next %u -- tail %u -- thread id %u\n", 
+		// printf("publish idx %u -- claimed %u -- head %u -- next %u -- tail %u -- thread id %u\n", 
 			idx, _claimed , _head, _next, tail, std::this_thread::get_id());
 		#endif
 
 		std::this_thread::yield();		        
-		mutex_lock.unlock();
 		return 0;
 
 	}
 private:
 	T *data;
 	std::mutex mutex_lock;
-	std::atomic<uint> claimed;
-	std::atomic<uint> next_to_be_claimed;
-	std::atomic<uint> head; 
-	volatile uint tail;
+	// std::atomic<uint> claimed;
+	// std::atomic<uint> next_to_be_claimed;
+	// std::atomic<uint> head; 
+	uint claimed;
+	uint next_to_be_claimed;
+	uint head; 
+	uint tail;
 	uint ring_size;
 	uint ring_mask;
 	uint mask; 
